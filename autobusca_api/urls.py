2f25a6c0c2daaf64db1125405425405747f9f8c4
kadu91878 <kadu_91878@hotmail.com>
2024-04-19 22:14:43 -0300

    feat: listar-placas

--- autobusca_api/urls.py
+++ autobusca_api/urls.py
@@ -1,21 +1,27 @@
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls.static import static
 
+from views.imagens_load_view import carregar_imagens
 from views.imagens_view import (ImagemListCreateAPIView,
-                                ImagemRetrieveUpdateDestroyAPIView)
+                                ImagemRetrieveUpdateDestroyAPIView,
+                                ListaImagemView)
 from views.teste_view import TesteView
 from views.veiculo_localizacao_view import (VeiculoLocalizacaoAPIView,
                                             VeiculoLocalizacaoByIdView)
-from views.imagens_load_view import carregar_imagens
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/veiculo-detail', VeiculoLocalizacaoAPIView.as_view(), name='veiculo-detail'),
+    path('api/veiculo-detail', VeiculoLocalizacaoAPIView.as_view(),
+         name='veiculo-detail'),
     path('api/teste', TesteView.as_view(), name='teste'),
-    path('api/veiculo-detail/<int:id>', VeiculoLocalizacaoByIdView.as_view(), name='veiculo-detail-id'),
+    path('api/veiculo-detail/<int:id>',
+         VeiculoLocalizacaoByIdView.as_view(), name='veiculo-detail-id'),
     path('api/imagens', ImagemListCreateAPIView.as_view(), name='imagens'),
-    path('api/imagens/<int:pk>', ImagemRetrieveUpdateDestroyAPIView.as_view(), name='imagens-id'),
-    path('api/carregar-imagens/<str:placa>/', carregar_imagens, name='imagens_por_placa'),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('api/imagens/<int:pk>',
+         ImagemRetrieveUpdateDestroyAPIView.as_view(), name='imagens-id'),
+    path('api/carregar-imagens/<str:placa>/',
+         carregar_imagens, name='imagens_por_placa'),
+    path('api/listar-placas', ListaImagemView.as_view(), name='listar-placas'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- views/imagens_view.py
+++ views/imagens_view.py
@@ -1,5 +1,9 @@
 from rest_framework import generics
 
+from django.db import connection
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 from models.imagens_model import Imagem
 from serializers.imagens_serializer import ImagemSerializer
 
@@ -7,7 +11,21 @@ from serializers.imagens_serializer import ImagemSerializer
 class ImagemListCreateAPIView(generics.ListCreateAPIView):
     queryset = Imagem.objects.all()
     serializer_class = ImagemSerializer
-    
+
+
 class ImagemRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Imagem.objects.all()
     serializer_class = ImagemSerializer
+
+
+class ListaImagemView(APIView):
+    def get(self, request,):
+        with connection.cursor() as cursor:
+            cursor.execute(
+                """select distinct i.placa_carro  from imagens i 
+                """,
+            )
+
+            results = cursor.fetchall()
+
+            return Response(results)
